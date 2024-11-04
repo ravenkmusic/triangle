8aebccf655c8d8e7b9e1e5cdda6dcf3d148abe44
Raven Hendricks <ravenkbeats@gmail.com>
2024-11-04 15:05:47 -0500

    continued scalene function

--- triangle.py
+++ triangle.py
@@ -3,3 +3,21 @@ def equilateral(sides):
         return True
     else:
         return False
+    
+
+def isosceles(sides):
+    if sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2]:
+        return True
+    if sides[0] + sides[1] <= sides[2]:
+        return False
+    if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2]:
+        return False
+    else:
+        return False
+
+
+def scalene(sides):
+    if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2] and sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
+        return True
+    else:
+        return False
