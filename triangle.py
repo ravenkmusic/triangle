e630ee6d91bbfa910c736f3bee20a1c5b2e82d87
Raven Hendricks <ravenkbeats@gmail.com>
2024-11-04 15:24:25 -0500

    modified isoceles function

--- triangle.py
+++ triangle.py
@@ -6,15 +6,12 @@ def equilateral(sides):
     
 
 def isosceles(sides):
-    if sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2]:
+    if sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2] and sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
         return True
-    if sides[0] + sides[1] <= sides[2]:
-        return False
     if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2]:
         return False
     else:
         return False
-
 
 def scalene(sides):
     if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2] and sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
