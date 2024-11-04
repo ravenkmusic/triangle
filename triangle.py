10e3cb1339b0e3caeae216135d6f772d049d914c
Raven Hendricks <ravenkbeats@gmail.com>
2024-11-04 16:39:01 -0500

    inserted is_triangle function in isoceles function

--- triangle.py
+++ triangle.py
@@ -12,13 +12,12 @@ def is_triangle(sides):
     
 
 def isosceles(sides):
-    if sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2] and sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
+    if sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2] and is_triangle(sides):
         return True
     if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2]:
         return False
     else:
         return False
-
 
 
 def scalene(sides):
